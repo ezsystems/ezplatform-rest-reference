b3bed5166350ebd78011662b61769536ea72fece
Tomasz Kryszan <tomasz.kryszan@ez.no>
2020-05-28 11:33:34 +0200

    TOC navigation improvements

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -21,21 +21,42 @@ $(function () {
                 && parentAnchor.classList.contains('active')) {
                 event.target.classList.toggle('toggler--rotated-0');
                 event.target.classList.remove('toggler--rotated');
+            } else if(event.target.classList.contains('toggler--rotated')
+                && parentAnchor.classList.contains('active')) {
+                event.target.classList.add('toggler--rotated-0');
+                event.target.classList.remove('toggler--rotated');
             } else {
                 event.target.classList.toggle('toggler--rotated');
             }
 
             if (parentAnchor.classList.contains('active')) {
+                subMenu.classList.remove('d-block');
                 subMenu.classList.toggle('d-none');
+
             } else {
+                subMenu.classList.remove('d-none');
                 subMenu.classList.toggle('d-block');
             }
         });
     });
 
     $(window).on('activate.bs.scrollspy', function (e, obj) {
-        const link = document.querySelector(`.nav-link[href="${obj.relatedTarget}"]`);
         const navBar = document.querySelector('.sidebar__nav');
+        const link = navBar.querySelector(`.nav-link[href="${obj.relatedTarget}"]`);
+        const activeRootNode = navBar.querySelector('.nav-link.active');
+
+        activeRootNode.childNodes.forEach(childNode => {
+            console.log(childNode.classList);
+
+            if (typeof childNode.classList !== "undefined"
+                && childNode.classList.contains('nav__link--toggler')) {
+                childNode.classList.remove('toggler--rotated-0');
+            }
+        });
+
+        if (activeRootNode.nextElementSibling.classList.contains('d-none')) {
+            activeRootNode.nextElementSibling.classList.remove('d-none');
+        }
 
         navBar.scrollTop = link.offsetTop;
     });
